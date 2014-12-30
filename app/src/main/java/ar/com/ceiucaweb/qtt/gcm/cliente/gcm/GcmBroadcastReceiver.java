d92c296fd233828d8a9ddbfd031e2c8c0a645e3b
juanmougan <juanmougan@gmail.com>
2014-12-30 19:19:13 -0300

    Commit inicial, a partir del ejemplo de la documentacion

---
+++ app/src/main/java/ar/com/ceiucaweb/qtt/gcm/cliente/gcm/GcmBroadcastReceiver.java
@@ -0,0 +1,23 @@
+package ar.com.ceiucaweb.qtt.gcm.cliente.gcm;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Ejemplo de BroadcastReceiver que recibe los mensajes e invoca al servicio que los procesará.
+ * Creado el 30/12/2014.
+ */
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that GcmIntentService will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                GcmIntentService.class.getName());
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
